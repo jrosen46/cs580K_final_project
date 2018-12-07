6b302f31792bc1a1e21bb8f7498b62c773312b23
Jared Rosenberg <jaredcolerosenberg@gmail.com>
2018-12-07 05:27:42 -0500

    Fix syntax error.

--- kubernetes/start_disk.sh
+++ kubernetes/start_disk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # old storage files
-#kubectrl create -f ./storage/storage_class-gce-fast.yaml
-#kubectrl create -f ./storage/nfs-server-pvc.yaml
-#kubectrl create -f ./storage/nfs-server-rc.yaml
-#kubectrl create -f ./storage/nfs-server-service.yaml
+#kubectl create -f ./storage/storage_class-gce-fast.yaml
+#kubectl create -f ./storage/nfs-server-pvc.yaml
+#kubectl create -f ./storage/nfs-server-rc.yaml
+#kubectl create -f ./storage/nfs-server-service.yaml
 
 # new storage files
-kubectrl create -f ./storage/fastStorageClass.yaml
-kubectrl create -f ./storage/claim.yaml
-kubectrl create -f ./storage/repControl.yaml
-kubectrl create -f ./storage/service.yaml
+kubectl create -f ./storage/fastStorageClass.yaml
+kubectl create -f ./storage/claim.yaml
+kubectl create -f ./storage/repControl.yaml
+kubectl create -f ./storage/service.yaml
